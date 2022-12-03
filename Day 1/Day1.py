3d19be1c063809c7fb4c69b9719ef56973157474
Dylan <dsja612@gmail.com>
2022-12-03 08:38:36 +0800

    Solved both parts of day 1

--- Day 1/Day1.py
+++ Day 1/Day1.py
@@ -1,18 +1,27 @@
 import os
+from queue import PriorityQueue
+
 path = "C:\\Users\\dsja6\\OneDrive - National University of Singapore\\Projects\\AdventOfCode2022\\Day 1"
 os.chdir(path)
 
 input = open("input.txt", "r")
 
-max = 0
+pq = PriorityQueue()
 cur = 0
 
 for line in input:
     if line != "\n":
         cur += int(line)
     else:
-        if cur > max:
-            max = cur
+        # Invert priority
+        pq.put(cur * -1, cur)
         cur = 0
 
-print(max)
+# Part 1
+print(pq.queue[0] * -1)
+
+# Part 2
+sum = 0
+for i in range(3):
+    sum += pq.get() * -1
+print(sum)
